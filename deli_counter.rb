882df7878cbe53dec9128e2c46324f2211a98a56
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-01 03:52:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ def line(array)
   if array.length != nil
     array.each_with_index do |name, index|
     puts "The line is currently: #{index+1}. #{name}"
+  end
   else
     puts "The line is currently empty."
   end
