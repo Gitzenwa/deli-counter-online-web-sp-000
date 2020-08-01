e5809ef32159308863c7f37e1c1b99b1cc486028
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-01 04:06:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,6 +14,6 @@ end
 def take_a_number(array, name)
   if array.length <= 0
     array << name
-    puts "Welcome #{name} you are number #{array.length} in line."
+    puts "Welcome #{name}, You are number #{array.length} in line."
   end
 end
