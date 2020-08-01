314420da88b330b8f70f616017b1b76f23e1cc4a
Ezenwa Onukwue <eonukwue@gmail.com>
2020-08-01 03:57:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 def line(array)
   if array.length > 0
+    line = []
     array.each_with_index do |name, index|
       puts "The line is currently: #{index+1}. #{name}"
     end
